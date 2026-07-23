bba49cda294b63fdf31d17a77706c01a5147f71c
Konstantin M <konmel96@gmail.com>
2026-07-24 02:16:34 +0300

    bug fix

--- guitar-frontend/src/middleware.ts
+++ guitar-frontend/src/middleware.ts
@@ -1,7 +1,34 @@
+// import createMiddleware from "next-intl/middleware";
+// import { routing } from "./i18n/routing";
+
+// export default createMiddleware(routing);
+
+// export const config = {
+//   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+// };
+
 import createMiddleware from "next-intl/middleware";
+import { NextResponse } from "next/server";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const handleI18nRouting = createMiddleware(routing);
+
+export default function middleware(request) {
+  const response = handleI18nRouting(request);
+
+  if (response.headers.has("location")) {
+    const location = response.headers.get("location");
+
+    if (location) {
+      const url = new URL(location);
+      url.host = request.headers.get("x-forwarded-host") || request.headers.get("host") || url.host;
+
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return response;
+}
 
 export const config = {
   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
