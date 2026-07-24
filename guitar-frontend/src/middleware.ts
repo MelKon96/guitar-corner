f6731609cf264ca801dbdc9709672ee61bf8a23a
Konstantin M <konmel96@gmail.com>
2026-07-24 03:10:44 +0300

    redirect fix

--- guitar-frontend/src/middleware.ts
+++ guitar-frontend/src/middleware.ts
@@ -5,16 +5,21 @@
 
 // export const config = {
 //   matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
+
+import createMiddleware from "next-intl/middleware";
 import { NextRequest, NextResponse } from "next/server";
+import { routing } from "./i18n/routing";
+
+const handleI18nRouting = createMiddleware(routing);
 
 export default function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/en", "https://guitar-corner-frontend.onrender.com"));
+    return NextResponse.redirect(new URL("/en", "https://guitarcornerfrontend.onrender.com"));
   }
 
-  return NextResponse.next();
+  return handleI18nRouting(request);
 }
 
 export const config = {
-  matcher: ["/"],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
